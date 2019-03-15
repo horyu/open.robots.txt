1c8ae60f15979bf8d3ec6306f693d0ddf6d0256a
horyu <horyu@users.noreply.github.com>
2019-03-15 11:25:57 +0900

    activate tab by requested url

--- background.js
+++ background.js
@@ -1,10 +1,21 @@
 chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
-    const now_url = tabs[0]['url'];
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs0) => {
+    const now_url = tabs0[0]['url'];
+    // https?:// で始まらなければ終了
     const rs = now_url.match(/^(https?:\/\/[^\/]+)/);
-    if (rs) {
-      const open_url = rs[1] + '/robots.txt';
-      window.open(open_url);
+    if (!rs) {
+      return;
     }
+    
+    const open_url = rs[1] + '/robots.txt';
+    // 開くべきURLのタブが存在する ? そのタブをactivate : 新しいタブをopen
+    chrome.tabs.query({'url': open_url}, (tabs1) => {
+      const tab = tabs1[0];
+      if (tab) {
+        chrome.tabs.update(tab.id, {'active': true});
+      } else {
+        window.open(open_url);
+      }
+    });
   });
 });
